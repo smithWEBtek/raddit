045288793e08a28ba291a979f4848e1902baeec9
James <james.randolph2016@gmail.com>
2018-10-29 20:15:39 -0400

    upvote and downvote in appropriate views

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -67,6 +67,13 @@ class LinksController < ApplicationController
     redirect_to :back
   end
 
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_by current_user
+    redirect_to :back
+  end
+  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
